6be513a6aaf2410d15044212022a2dcbd0598921
Dushyant-singh10 <ds0958570@gmail.com>
2026-08-26 13:46:03 +0530

    Add migration for org/user photo and bio columns

---
+++ drizzle/0004_true_hellcat.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "organizations" ADD COLUMN "photo_url" text;--> statement-breakpoint
+ALTER TABLE "organizations" ADD COLUMN "about" text;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "photo_url" text;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "bio" text;
